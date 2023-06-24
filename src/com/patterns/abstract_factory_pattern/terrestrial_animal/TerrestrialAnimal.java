25fa065169bd8c2f7b12e19922be3a1d10eb2292
Alex Poma <alex.rpoma@outlook.com>
2023-06-24 12:37:13 -0400

    Some clean code

--- src/com/patterns/abstract_factory_pattern/terrestrial_animal/TerrestrialAnimal.java
+++ src/com/patterns/abstract_factory_pattern/terrestrial_animal/TerrestrialAnimal.java
@@ -3,7 +3,7 @@ package com.patterns.abstract_factory_pattern.terrestrial_animal;
 public interface TerrestrialAnimal {
   default String generalInfo(){
     return "It is considered a wild, non-domesticated animal.";
-  };
+  }
   String makeSound();
   String run();
   String meal();
--- src/com/patterns/abstract_factory_pattern/terrestrial_animal/TerrestrialAnimalFactory.java
+++ src/com/patterns/abstract_factory_pattern/terrestrial_animal/TerrestrialAnimalFactory.java
@@ -4,17 +4,15 @@ import com.patterns.abstract_factory_pattern.AbstractAnimalFactory;
 
 public class TerrestrialAnimalFactory implements AbstractAnimalFactory<TerrestrialAnimal> {
 
-  private static TerrestrialAnimal terrestrialAnimal;
 
   @Override
   public TerrestrialAnimal create(String animal) {
     if (animal.equals("Dog")) {
-      terrestrialAnimal = new Dog();
+      return new Dog();
     }
     else if (animal.equals("Cat")) {
-      terrestrialAnimal = new Cat();
+      return new Cat();
     }
     else throw new IllegalArgumentException("Anima type doesn't exist!");
-    return terrestrialAnimal;
   }
 }
