538d47fbb8497facd4d3e061025b868a9053e047
Alex Poma <alex.rpoma@outlook.com>
2023-06-27 09:55:31 -0400

    Singleton pattern

---
+++ src/com/patterns/singleton_pattern/App.java
@@ -0,0 +1,14 @@
+package com.patterns.singleton_pattern;
+
+public class App {
+  public static void main(String[] args) {
+    DataBaseConnection dataBaseConnection = DataBaseConnection.getInstance();
+    String info = dataBaseConnection.showInfo();
+    System.out.println(info);
+    System.out.printf("dataBaseConnection hashcode: %s%n", dataBaseConnection.hashCode());
+
+    DataBaseConnection dataBaseConnection1 = DataBaseConnection.getInstance();
+    System.out.printf("dataBaseConnection1 hashcode: %s%n", dataBaseConnection1.hashCode());
+    System.out.printf("Both of them are equals: %s", dataBaseConnection.equals(dataBaseConnection1));
+  }
+}
